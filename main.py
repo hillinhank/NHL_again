6972429a7cf12ada6d6e14c1bfefd59d9e51d26a
hillinhank <101481759+hillinhank@users.noreply.github.com>
2023-01-04 06:11:56 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
+names = {"tim":{"age":78, "gender": "male"},
+        "bill": {"age":68, "gender": "male"}}
+
+
 # make a resource in this api, named HelloWorld 
 # overridden the get method i.e. this is what will happen when send a get request to a specific url
 # when that happens, we will return Hello World!
 class HelloWorld(Resource):
-    def get(self, name, test):
-        return {"name": name, "test": test}
+    def get(self, name):
+        return names[name]
 # now need to register this as a resource api.add_resource(Class, url) (can use a / to use a default url)
 # essentially, it determines what the root of the resource will be
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 # this starts both the server and the application
 # debug=True says that we are in debug mode so we will see all of that output and any logging information, so if we have a bug it will tell us why
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 # send a request to the url that is base url plus helloworld
-respone = requests.get(BASE + "helloworld/hank/30")
+respone = requests.get(BASE + "helloworld/tim")
 print(respone.json())

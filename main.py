9c2b913613ab6fd2b00f55771d87fe2c436e76fe
hillinhank <101481759+hillinhank@users.noreply.github.com>
2023-01-04 06:12:27 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 #! /var/mail/flask
 from flask import Flask
-#, request
-from flask_restful import Resource, Api
-#, abort, reqparse
+from flask_restful import Resource, Api, abort, reqparse
 
 # this wraps our app in an api and this initializes a restful api
 app = Flask(__name__)
 api = Api(app)
 
+# make a new request parser object
+## will automatically parse through requests that are being sent to make sure it fits the guidelines we choose
+video_put_args = reqparse.RequestParser()
+# 
+video_put_args.add_argument("name", type=str, help="name of the video is required", required = True)
+video_put_args.add_argument("views", type=int, help="views of the video is required", required = True)
+video_put_args.add_argument("likes", type=int, help="likes of the video is required", required = True)
+videos = {}
 
-names = {"tim":{"age":78, "gender": "male"},
-        "bill": {"age":68, "gender": "male"}}
+def ab_if_vid_not(video_id):
+    if video_id not in videos:
+        abort(404, message = "Video id is not valid...")
+
+def ab_if_vid_in(video_id):
+    if video_id in videos:
+        abort(409, message = "Video already in catalog...")
+# make a resource in this api, 
+# when a get request is sent via .add_resource information will be sent based on the video_id I have asked for
+class Video(Resource):
+    def get(self, video_id):
+        ab_if_vid_not(video_id)
+        return videos[video_id]
+
+    def put(self, video_id):
+        ab_if_vid_in(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id]= args
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        ab_if_vid_not(video_id)
+        del videos[video_id]
+        return '', 204
 
 
-# make a resource in this api, named HelloWorld 
-# overridden the get method i.e. this is what will happen when send a get request to a specific url
-# when that happens, we will return Hello World!
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
 # now need to register this as a resource api.add_resource(Class, url) (can use a / to use a default url)
 # essentially, it determines what the root of the resource will be
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 # this starts both the server and the application
 # debug=True says that we are in debug mode so we will see all of that output and any logging information, so if we have a bug it will tell us why
--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-# send a request to the url that is base url plus helloworld
-respone = requests.get(BASE + "helloworld/tim")
-print(respone.json())
+
+data = [{"likes":10, "name":"words", "views": 143245},
+{"likes":378899, "name":"how to develop python", "views": 111},
+{"likes":4389, "name":"Tim sings", "views": 133333},
+{"likes":1050, "name":"Tim doesnt", "views": 1222222}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+
+
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
+print(response.json())

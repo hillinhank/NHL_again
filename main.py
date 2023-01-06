cf4080ff0a0871a05383c659bb04447b060af2fc
hillinhank <101481759+hillinhank@users.noreply.github.com>
2023-01-06 12:53:31 -0600

    /Users/user/Desktop/CLSSFOLDER/REST/main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 #! /var/mail/flask
 from flask import Flask
-from flask_restful import Resource, Api, abort, reqparse
+from flask_restful import Resource, Api, abort, reqparse, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 # this wraps our app in an api and this initializes a restful api
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video (name = {name}, views = {views}, likes = {likes})"
+        
+
+# db.create_all() COMMENT OUT create_all so we dont override that data in our db
 
 # make a new request parser object
 ## will automatically parse through requests that are being sent to make sure it fits the guidelines we choose
@@ -13,27 +28,39 @@ video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="name of the video is required", required = True)
 video_put_args.add_argument("views", type=int, help="views of the video is required", required = True)
 video_put_args.add_argument("likes", type=int, help="likes of the video is required", required = True)
-videos = {}
 
-def ab_if_vid_not(video_id):
-    if video_id not in videos:
-        abort(404, message = "Video id is not valid...")
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="name of the video is required")
+video_update_args.add_argument("views", type=int, help="views of the video is required")
+video_update_args.add_argument("likes", type=int, help="likes of the video is required")
 
-def ab_if_vid_in(video_id):
-    if video_id in videos:
-        abort(409, message = "Video already in catalog...")
+resource_fields = {
+    'id':fields.Integer,
+    'name':fields.String,
+    'views':fields.Integer,
+    'likes':fields.Integer
+}
+
 # make a resource in this api, 
 # when a get request is sent via .add_resource information will be sent based on the video_id I have asked for
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        ab_if_vid_not(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message= "no video found")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        ab_if_vid_in(video_id)
         args = video_put_args.parse_args()
-        videos[video_id]= args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message = "Video is taken...")
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
 
     def delete(self, video_id):
         ab_if_vid_not(video_id)
@@ -43,7 +70,7 @@ class Video(Resource):
 
 # now need to register this as a resource api.add_resource(Class, url) (can use a / to use a default url)
 # essentially, it determines what the root of the resource will be
-api.add_resource(Video, "/video/<int:video_id>")
+api.add_resource(Video, "/https://statsapi.web.nhl.com/api/v1/")
 
 # this starts both the server and the application
 # debug=True says that we are in debug mode so we will see all of that output and any logging information, so if we have a bug it will tell us why
--- test.py
+++ test.py
@@ -1,20 +1,8 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/"
-
-data = [{"likes":10, "name":"words", "views": 143245},
-{"likes":378899, "name":"how to develop python", "views": 111},
-{"likes":4389, "name":"Tim sings", "views": 133333},
-{"likes":1050, "name":"Tim doesnt", "views": 1222222}]
-
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+BASE = "https://statsapi.web.nhl.com/api/v1/"
 
 
-
-response = requests.delete(BASE + "video/0")
-print(response)
-input()
-response = requests.get(BASE + "video/2")
+response = requests.put(BASE + "teams")
+#{"views":99, "likes":101})
 print(response.json())
